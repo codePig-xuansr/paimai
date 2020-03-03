11ce87b8bf0c29d47dc065c0995c750ad6fa27ca
xuansr <842704182@qq.com>
2020-03-03 14:17:45 +0800

    springboot结构提交

---
+++ src/main/java/com/accp/paimai/cfg/BeansConfig.java
@@ -0,0 +1,13 @@
+package com.accp.paimai.cfg;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@MapperScan(basePackages = { "com.accp.dao" })
+public class BeansConfig {
+	
+
+}
